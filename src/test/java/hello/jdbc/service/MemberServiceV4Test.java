370ae4963a537c8e2922fe26e9295c1f30ed826a
kimyoungwoong <kyw04004@naver.com>
2022-04-24 18:29:40 +0900

    JdbcTemplate

--- src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -4,6 +4,7 @@ import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
 import hello.jdbc.repository.MemberRepositoryV4_1;
 import hello.jdbc.repository.MemberRepositoryV4_2;
+import hello.jdbc.repository.MemberRepositoryV5;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -48,7 +49,8 @@ class MemberServiceV4Test {
         @Bean
         MemberRepository memberRepository() {
             //return new MemberRepositoryV4_1(dataSource);
-            return new MemberRepositoryV4_2(dataSource);
+            //return new MemberRepositoryV4_2(dataSource);
+            return new MemberRepositoryV5(dataSource);
         }
         @Bean
         MemberServiceV4 memberServiceV4() {
